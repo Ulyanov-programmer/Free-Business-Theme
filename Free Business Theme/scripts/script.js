9d175d9a5ab370b95276cf88e3eb41561b312993
Ulyanov Ivan <ccoldatheinrich@yandex.ru>
2021-06-29 11:24:31 +0400

    first commit

---
+++ Free Business Theme/scripts/script.js
@@ -0,0 +1,79 @@
+'use strict'
+
+let body = document.body;
+let innerWindowWigth = window.innerWidth;
+let innerWindowHeight = window.innerWidth;
+
+// ? If you see an error here, it's normal.
+
+class IsMobile {
+    static Android() {
+        return navigator.userAgent.match(/Android/i);
+    }
+    static BlackBerry() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    }
+    static iOS() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    }
+    static Opera() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    }
+    static Windows() {
+        return navigator.userAgent.match(/IEMobile/i);
+    }
+    static any() {
+        return (
+            this.Android() || this.BlackBerry() ||
+            this.iOS() || this.Opera() || this.Windows()
+        );
+    }
+}
+// ? IsMobile before ECMAScript 2015
+/* const isMobile = {
+    Android: function () { return navigator.userAgent.match(/Android/i); },
+    BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); },
+    iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); },
+    Opera: function () { return navigator.userAgent.match(/Opera Mini/i); },
+    Windows: function () { return navigator.userAgent.match(/IEMobile/i); },
+    any: function () { return ( isMobile.Android() || isMobile.BlackBerry() ||
+            isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); }
+ }; */
+
+if (IsMobile.any()) {
+    body.classList.add('_touch');
+} else {
+    body.classList.add('_pc');
+};
+
+function showOrHideSubmenu(e) {
+    const submenu = document.querySelector('.navmenu__submenu');
+
+    if (submenu !== undefined) {
+        activateSubmenuButton.classList.toggle('active');
+        submenu.classList.toggle('_active');
+    }
+}
+const activateSubmenuButton = document.getElementById('activate-submenu-button');
+activateSubmenuButton.addEventListener('click', showOrHideSubmenu);
+
+
+function headerToFixed(e) {
+    // Calculating the degree of scrolling in pixels, 
+    // multiply the innerWindowHeight by the desired scrolling percentage as 0.percent.
+    /* Example:
+        25 percent of innerWindowHeight = innerWindowHeight * 0.25 
+        5 percent of 700 = 700 * 0.05
+    */
+    var scrollPercentage = innerWindowHeight * 0.15;
+
+    if (pageYOffset > scrollPercentage) {
+        burger.classList.add('burger-black');
+        header.classList.add('fixed-header');
+    } else {
+        burger.classList.remove('burger-black');
+        header.classList.remove('fixed-header');
+    }
+}
+const header = document.querySelector('.header__body');
+window.addEventListener('scroll', headerToFixed);
---
+++ README.md
@@ -0,0 +1,44 @@
+# LeRestaurante
+Free Business Theme - business website made on the basis of the design layout.
+
+## How to launch?
+<details>
+<summary>VS Code</summary>
+
+ 1. Copy the path to the repository, **not forgetting to specify the branch you are interested in**. ![howToLaunch_vsc1](/readmeFiles/howToLaunchVSCode/howToLaunch_vsc1.png "How to load")
+ 2. Start Visual Studio Code, where by selecting the Explorer item, select **Clone repository**. ![howToLaunch_vsc1](/readmeFiles/howToLaunchVSCode/howToLaunch_vsc2.png)
+ 3. In the pop-up window, enter the previously copied path to the repository and select the folder where you want to save the files. ![howToLaunch_vsc1](/readmeFiles/howToLaunchVSCode/howToLaunch_vsc3.png)
+ - *Make sure that the path to the project folder is specified in the terminal!*
+ - *Make sure you have Python installed (most likely you will need version 2.7, if a newer one is installed, you may have to uninstall it).*
+ 4. Enter terminal (**ctrl + j** or **view** -> **terminal**) ``npm i`` command.
+ 
+ 5. If there are no errors in the process, you will see something like this: ![howToLaunch_vsc1](/readmeFiles/howToLaunchVSCode/howToLaunch_vsc4.png)
+ 6. Start Galp from the terminal by entering the galp command.
+If this command doesn't work (you probably wouldn't find it), try installing **gulp-cli** via ``npm -g install gulp-cli``.
+A window with the main page of the project will open in the browser by default (*if it does not load correctly, you may need to update it using* ***ctrl + f5***).
+
+</details>
+
+<details>
+<summary>Just in a browser</summary>
+
+
+Most likely, you will be given the folder with the project you are interested in in a different way, but if you are still interested in:
+1. Download the project **via ZIP**. ![howToLaunch_vsc1](/readmeFiles/howToLaunchBrowser/howToLaunch_browser1.png)
+2. Open the archive and find the folder with the final files (most likely it will not be named #src), extract it to your PC.
+If you're interested, you can download a mockup of the original design.
+![howToLaunch_vsc1](/readmeFiles/howToLaunchBrowser/howToLaunch_browser2.png)
+3. Run the **index.html** file in your browser.
+
+</details>
+
+<details>
+<summary>GitHub Pages (recommented)</summary>
+
+You can run the project in the browser without downloading.
+
+To do this, open this link: https://ulyanov-programmer.github.io/LeRestaurante/LeRestaurante/index (i recommend disabling external modifiers for page properties, such as *dark-reader*.)
+
+</details>
+
+If you have any questions or suggestions, write to me by mail: ccoldatheinrich@yandex.ru
